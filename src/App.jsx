5100b35b40f3097d6644b30e89bd9b01a75c6ddc
Danila-A <dan4ik.rus@mail.ru>
2024-10-30 11:48:05 +0300

    I finished the main banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import Header from "./components/Header/Header"
 import MainBanner from "./components/MainBanner/MainBanner"
 
 const App = () => {
   return (
     <>
-      <div className="container">
-        {/* <Header /> */}
-      </div>
       <MainBanner />
     </>
   )
---
+++ src/components/BackgroundImage/BackgroundImage.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+const BackgroundImage = () => {
+    return (
+        <img className={ styles.backgroundImage } src="https://assets.nflxext.com/ffe/siteui/vlv3/74d734ca-0eab-4cd9-871f-bca01823d872/web/NL-nl-20241021-TRIFECTA-perspective_394181aa-685c-4788-a8ca-4bc669d692ba_large.jpg" />
+    );
+}
+
+export default BackgroundImage;
